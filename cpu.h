38b6b431abe49c137ad9d6f10df18d6a22925a6e
Ramon <Ramonlinke@gmail.com>
2025-12-21 19:46:02 +0100

    defines opcode names as string with opcode byte length

--- cpu.h
+++ cpu.h
@@ -3,6 +3,7 @@
 
 #include <math.h>
 #include <functional>
+#include <string>
 
 #include "memory.h"
 #include "defines.h"
@@ -400,6 +401,7 @@ private:
     void CPI(Memory* mem);
     void RST7(Memory* mem) { RST(mem, 7); }
 
+    // contains a function pointer to the opcode handler
     struct CPUOpcode
     {
         void (CPU::*handler)(Memory* data);
@@ -440,6 +442,49 @@ private:
     //  0xF0          0xF1          0xF2          0xF3          0xF4          0xF5          0xF6          0xF7          0xF8          0xF9          0xFA          0xFB          0xFC          0xFD          0xFE          0xFF
         &CPU::RP,     &CPU::POP_PSW,&CPU::JP,     &CPU::DI,     &CPU::CP,     &CPU::PUSH_PSW,&CPU::ORI,   &CPU::RST6,   &CPU::RM,     &CPU::SPHL,   &CPU::JM,     &CPU::EI,     &CPU::CM,     &CPU::ILL,    &CPU::CPI,    &CPU::RST7,
     };
+
+    // contains the opcode as String and the amount of bytes read at PC by the opcode
+    struct OpcodeData
+    {
+        std::string name;   // opcode name in dissasembly
+        uint8 length;       // opcode length in bytes
+    };
+
+    OpcodeData opcodeData[0x100] = {
+    //   0x00           0x01           0x02           0x03           0x04           0x05           0x06           0x07           0x08           0x09           0x0A           0x0B           0x0C           0x0D           0x0E           0x0F
+        {"NOP", 1},    {"LXI_B", 3},  {"STAX_B", 1}, {"INX_B", 1},  {"INR_B", 1},  {"DCR_B", 1},  {"MVI_B", 2},  {"RLC", 1},    {"ILL", 1},    {"DAD_B", 1},  {"LDAX_B", 1}, {"DCX_B", 1},  {"INR_C", 1},  {"DCR_C", 1},  {"MVI_C", 2},  {"RRC", 1},
+    //   0x10           0x11           0x12           0x13           0x14           0x15           0x16           0x17           0x18           0x19           0x1A           0x1B           0x1C           0x1D           0x1E           0x1F
+        {"ILL", 1},    {"LXI_D", 3},  {"STAX_D", 1}, {"INX_D", 1},  {"INR_D", 1},  {"DCR_D", 1},  {"MVI_D", 2},  {"RAL", 1},    {"ILL", 1},    {"DAD_D", 1},  {"LDAX_D", 1}, {"DCX_D", 1},  {"INR_E", 1},  {"DCR_E", 1},  {"MVI_E", 2},  {"RAR", 1},
+    //   0x20           0x21           0x22           0x23           0x24           0x25           0x26           0x27           0x28           0x29           0x2A           0x2B           0x2C           0x2D           0x2E           0x2F
+        {"ILL", 1},    {"LXI_H", 3},  {"SHLD", 3},   {"INX_H", 1},  {"INR_H", 1},  {"DCR_H", 1},  {"MVI_H", 2},  {"DAA", 1},    {"ILL", 1},    {"DAD_H", 1},  {"LHLD", 3},   {"DCX_H", 1},  {"INR_L", 1},  {"DCR_L", 1},  {"MVI_L", 2},  {"CMA", 1},
+    //   0x30           0x31           0x32           0x33           0x34           0x35           0x36           0x37           0x38           0x39           0x3A           0x3B           0x3C           0x3D           0x3E           0x3F
+        {"ILL", 1},    {"LXI_SP", 3}, {"STA", 3},    {"INX_SP", 1}, {"INR_M", 1},  {"DCR_M", 1},  {"MVI_M", 2},  {"STC", 1},    {"ILL", 1},    {"DAD_SP", 1}, {"LDA", 3},    {"DCX_SP", 1}, {"INR_A", 1},  {"DCR_A", 1},  {"MVI_A", 2},  {"CMC", 1},
+    //   0x40           0x41           0x42           0x43           0x44           0x45           0x46           0x47           0x48           0x49           0x4A           0x4B           0x4C           0x4D           0x4E           0x4F
+        {"MOV_BB", 1}, {"MOV_BC", 1}, {"MOV_BD", 1}, {"MOV_BE", 1}, {"MOV_BH", 1}, {"MOV_BL", 1}, {"MOV_BM", 1}, {"MOV_BA", 1}, {"MOV_CB", 1}, {"MOV_CC", 1}, {"MOV_CD", 1}, {"MOV_CE", 1}, {"MOV_CH", 1}, {"MOV_CL", 1}, {"MOV_CM", 1}, {"MOV_CA", 1},
+    //   0x50           0x51           0x52           0x53           0x54           0x55           0x56           0x57           0x58           0x59           0x5A           0x5B           0x5C           0x5D           0x5E           0x5F
+        {"MOV_DB", 1}, {"MOV_DC", 1}, {"MOV_DD", 1}, {"MOV_DE", 1}, {"MOV_DH", 1}, {"MOV_DL", 1}, {"MOV_DM", 1}, {"MOV_DA", 1}, {"MOV_EB", 1}, {"MOV_EC", 1}, {"MOV_ED", 1}, {"MOV_EE", 1}, {"MOV_EH", 1}, {"MOV_EL", 1}, {"MOV_EM", 1}, {"MOV_EA", 1},
+    //   0x60           0x61           0x62           0x63           0x64           0x65           0x66           0x67           0x68           0x69           0x6A           0x6B           0x6C           0x6D           0x6E           0x6F
+        {"MOV_HB", 1}, {"MOV_HC", 1}, {"MOV_HD", 1}, {"MOV_HE", 1}, {"MOV_HH", 1}, {"MOV_HL", 1}, {"MOV_HM", 1}, {"MOV_HA", 1}, {"MOV_LB", 1}, {"MOV_LC", 1}, {"MOV_LD", 1}, {"MOV_LE", 1}, {"MOV_LH", 1}, {"MOV_LL", 1}, {"MOV_LM", 1}, {"MOV_LA", 1},
+    //   0x70           0x71           0x72           0x73           0x74           0x75           0x76           0x77           0x78           0x79           0x7A           0x7B           0x7C           0x7D           0x7E           0x7F
+        {"MOV_MB", 1}, {"MOV_MC", 1}, {"MOV_MD", 1}, {"MOV_ME", 1}, {"MOV_MH", 1}, {"MOV_ML", 1}, {"HLT", 1},    {"MOV_MA", 1}, {"MOV_AB", 1}, {"MOV_AC", 1}, {"MOV_AD", 1}, {"MOV_AE", 1}, {"MOV_AH", 1}, {"MOV_AL", 1}, {"MOV_AM", 1}, {"MOV_AA", 1},
+    //   0x80           0x81           0x82           0x83           0x84           0x85           0x86           0x87           0x88           0x89           0x8A           0x8B           0x8C           0x8D           0x8E           0x8F
+        {"ADD_B", 1},  {"ADD_C", 1},  {"ADD_D", 1},  {"ADD_E", 1},  {"ADD_H", 1},  {"ADD_L", 1},  {"ADD_M", 1},  {"ADD_A", 1},  {"ADC_B", 1},  {"ADC_C", 1},  {"ADC_D", 1},  {"ADC_E", 1},  {"ADC_H", 1},  {"ADC_L", 1},  {"ADC_M", 1},  {"ADC_A", 1},
+    //   0x90           0x91           0x92           0x93           0x94           0x95           0x96           0x97           0x98           0x99           0x9A           0x9B           0x9C           0x9D           0x9E           0x9F
+        {"SUB_B", 1},  {"SUB_C", 1},  {"SUB_D", 1},  {"SUB_E", 1},  {"SUB_H", 1},  {"SUB_L", 1},  {"SUB_M", 1},  {"SUB_A", 1},  {"SBB_B", 1},  {"SBB_C", 1},  {"SBB_D", 1},  {"SBB_E", 1},  {"SBB_H", 1},  {"SBB_L", 1},  {"SBB_M", 1},  {"SBB_A", 1},
+    //   0xA0           0xA1           0xA2           0xA3           0xA4           0xA5           0xA6           0xA7           0xA8           0xA9           0xAA           0xAB           0xAC           0xAD           0xAE           0xAF
+        {"ANA_B", 1},  {"ANA_C", 1},  {"ANA_D", 1},  {"ANA_E", 1},  {"ANA_H", 1},  {"ANA_L", 1},  {"ANA_M", 1},  {"ANA_A", 1},  {"XRA_B", 1},  {"XRA_C", 1},  {"XRA_D", 1},  {"XRA_E", 1},  {"XRA_H", 1},  {"XRA_L", 1},  {"XRA_M", 1},  {"XRA_A", 1},
+    //   0xB0           0xB1           0xB2           0xB3           0xB4           0xB5           0xB6           0xB7           0xB8           0xB9           0xBA           0xBB           0xBC           0xBD           0xBE           0xBF
+        {"ORA_B", 1},  {"ORA_C", 1},  {"ORA_D", 1},  {"ORA_E", 1},  {"ORA_H", 1},  {"ORA_L", 1},  {"ORA_M", 1},  {"ORA_A", 1},  {"CMP_B", 1},  {"CMP_C", 1},  {"CMP_D", 1},  {"CMP_E", 1},  {"CMP_H", 1},  {"CMP_L", 1},  {"CMP_M", 1},  {"CMP_A", 1},
+    //   0xC0           0xC1           0xC2           0xC3           0xC4           0xC5           0xC6           0xC7           0xC8           0xC9           0xCA           0xCB           0xCC           0xCD           0xCE           0xCF
+        {"RNZ", 1},    {"POP_B", 1},  {"JNZ", 3},    {"JMP", 3},    {"CNZ", 3},    {"PUSH_B", 1}, {"ADI", 2},    {"RST0", 1},   {"RZ", 1},     {"RET", 1},    {"JZ", 3},     {"ILL", 3},    {"CZ", 3},     {"CALL", 3},   {"ACI", 2},    {"RST1", 1},
+    //   0xD0           0xD1           0xD2           0xD3           0xD4           0xD5           0xD6           0xD7           0xD8           0xD9           0xDA           0xDB           0xDC           0xDD           0xDE           0xDF
+        {"RNC", 1},    {"POP_D", 1},  {"JNC", 3},    {"OUT", 2},    {"CNC", 3},    {"PUSH_D", 1}, {"SUI", 2},    {"RST2", 1},   {"RC", 1},     {"ILL", 1},    {"JC", 3},     {"IN", 1},     {"CC", 3},     {"ILL", 3},    {"SBI", 2},    {"RST3", 1},
+    //   0xE0           0xE1           0xE2           0xE3           0xE4           0xE5           0xE6           0xE7           0xE8           0xE9           0xEA           0xEB           0xEC           0xED           0xEE           0xEF
+        {"RPO", 1},    {"POP_H", 1},  {"JPO", 3},    {"ILL", 1},    {"CPO", 3},    {"PUSH_H", 1}, {"ANI", 2},    {"RST4", 1},   {"RPE", 1},    {"PCHL", 1},   {"JPE", 3},    {"XCHG", 1},   {"CPE", 3},    {"ILL", 3},    {"XRI", 2},    {"RST5", 1},
+    //   0xF0           0xF1           0xF2           0xF3           0xF4           0xF5           0xF6           0xF7           0xF8           0xF9           0xFA           0xFB           0xFC           0xFD           0xFE           0xFF
+        {"RP", 1},     {"POP_PSW", 1},{"JP", 3},     {"DI", 1},     {"CP", 3},     {"PUSH_PSW", 1},{"ORI", 2},   {"RST6", 1},   {"RM", 1},     {"SPHL", 1},   {"JM", 3},     {"EI", 1},     {"CM", 3},     {"ILL", 3},    {"CPI", 2},    {"RST7", 1},
+    };
+
 };
 
 #endif
